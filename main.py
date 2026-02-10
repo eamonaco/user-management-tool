5032ed1328d54645ebdb9f84b48d6e5519cbfc80
Enzo <albomonaco@gmail.com>
2026-02-10 19:57:47 -0300

    feat: create base menu and program loop

---
+++ main.py
@@ -0,0 +1,42 @@
+def main():
+    quit_program = False
+    user_list = []
+    while not quit_program:
+        print(
+        """
+        ---------------------------------------------------------
+            |Selecione uma opção:
+            |1. Cadastrar usuário
+            |2. Listar usuários
+            |3. Atualizar usuário
+            |4. Remover usuário
+            |5. Sair
+        ---------------------------------------------------------
+        """)
+        answer = int(input('\nOpção: '))
+        
+       
+        if answer == 5:
+            quit_program = True
+        
+        elif answer == 1:
+            user_name = input('Digite o nome do usuário: ')
+            user_email = input('Digite o email do usuário: ')
+            user_age = int(input('Digite a idade do usuário: '))
+            
+            user = {'name': user_name, 
+                    'email': user_email,
+                    'age': user_age}
+
+            user_list.append(user)
+            
+            print('Usuário cadastrado com sucesso.')
+
+        elif answer in range(2,5):
+            print('\nOpção não implementada. Tente novamente em breve.')
+        else:
+            print('Opção inválida.')
+
+    print('\nPrograma finalizado.')
+
+main()  
